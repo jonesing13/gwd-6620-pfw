b1425b35d9ce359e8b5590267a76bf329047ff83
C Jones <cjones013@gmail.com>
2024-02-08 18:25:47 -0600

    wk4 exercise 1.1 enhancements

--- wk4/ex1.2/exercise2.js
+++ wk4/ex1.2/exercise2.js
@@ -5,6 +5,8 @@ let theBox = {
     y: 350
 }
 let mouseClicks = 0;
+const colors = ['cornflowerblue', 'tomato', 'purple', 'teal', 'blue', 'orange', 'yellow', 'red', 'green', 'magenta', 'olive', 'maroon', 'aqua', 'limegreen'];
+let colorIndex = 0;
 
 function setup() {
     createCanvas(500, 500);
@@ -12,33 +14,37 @@ function setup() {
 
 function draw() {
     background('lightblue');
+    fill('black');
+    textSize(18);
+    text('click the box to score points!', 40, 100);
     drawShape(); 
     theBox.y--; // since draw() is a loop, this moves the shape up on the y-axis by one each iteration
-    if(theBox.y < -theBox.height) { // my square is moving up, so I used the distance of the height, i.e. negative height 
+    if(theBox.y < -theBox.height) { // my square is moving up, so I used the distance of the height past the canvas as my condition, i.e. negative height 
         noLoop();
-        text('your score was ' + mouseClicks, 230, 240);
+        fill('blue');
+        text('nice! your score was: ' + mouseClicks, 230, 240);
     }
 }
 
 function mousePressed() {
     // console.log("mouse x", mouseX);
     // console.log("mouse y", mouseY);
-     if( // check if mouse click is inside the box
+     if( // check if mouse click is inside the box (success)
         (mouseX >= theBox.x && mouseX <= theBox.x + theBox.width) && (mouseY >= theBox.y && mouseY <= theBox.y + theBox.height)
     ) {
         mouseClicks++; // count successful mouse clicks
         console.log('you hit the box!', mouseClicks);
+
+        if(colorIndex < (colors.length -1)) { // checks to see if colorIndex is going to be in range. 6 colors, array only counts to 5
+            colorIndex ++
+        } else { // resets colorIndex so colors can keep changing
+            colorIndex = 0;
+        }
     } 
 }
 
 function drawShape() {
-    fill('blue');
+    fill(colors[colorIndex]);
+    noStroke();
     rect(theBox.x, theBox.y, theBox.width, theBox.height);
 }
-
-
-// After you have that working, create a variable to be used for either the shape's x or y. Increment this in the draw function so the shape appears to be moving. This should still detecting mouse clicks (i.e. parameterize your coordinates/inputs).
-
-// After you have this working, create a conditional in your draw function to detect if the shape is off the canvas. When this occurs, call 'noLoop' to stop the draw function from repeating. Display the number of clicks the user has done.
-
-// Feel free to make this interesting with adding color and description text.
