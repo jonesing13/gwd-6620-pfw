2e68d51eced0cdd5fbf0bdd0c0c367713c295d20
C Jones <cjones013@gmail.com>
2024-02-10 10:37:20 -0600

    week 4 shoring up some prep work

--- wk4/assignments/memory-game.js
+++ wk4/assignments/memory-game.js
@@ -1,7 +1,16 @@
 let startingX = 24;
 let startingY = 200;
-let cardWidth = 120;
-let cardHeight = 120;
+const cardWidth = 120;
+const cardHeight = 120;
+
+/*
+COLORS
+
+woodstock yellow #ffdc17, #ffdb18
+sky blue #95d0fc
+grass #909710
+dog house red #ea191e
+*/
 
 function setup() {
     createCanvas(848, 640);
@@ -10,25 +19,14 @@ function setup() {
 
 function draw() {
     noStroke();
-    noLoop();
-    // rect(16, 200, cardWidth, cardHeight);
-    rect(startingX, startingY, cardWidth, cardHeight); // allows for 6 colums at 120w w 16px gap/gutter. using same dimension for height cause my images are squares. 408h needed for the 3 rows (but this can allow for additional space for title and score copy)
-    // rect(16, 336, 120, 120);
-    // rect(16, 472, 120, 120);
-
-    /* for(let x = 24; x < width; x += 136) {
-        //rect(x, startingY, cardWidth, cardHeight); only if we're just doing 1 row
-        for(let y = 200; y < height; y += 136) { // to add more rows, need another for
-            rect(x, y, cardWidth, cardHeight);
-        }
-        
-    } */
+    noLoop(); // doing my own for loops!
+    rect(startingX, startingY, cardWidth, cardHeight); // allows for 6 colums at 120w w 16px gap/gutter. using same dimension for height cause my images are squares. 408h needed for the 3 rows (but this can allow for additional space for title and score copy, as well as padding at the bottom of the canvas for ~aesthetics~)
 
     // i have 18 cards, so 3 rows of 6 columns
     // set for loop to handle rows
     for(let rows = 0; rows < 3; rows++) {
         // inner loop handles columns
-        for(let columns = 0; columns < 6; columns++) { // with this, it's ideal to set a variable for the x and y, like how we changed the box dimensions to be set outside of the loops/elements themselves
+        for(let columns = 0; columns < 6; columns++) { 
             rect(startingX, startingY, cardWidth, cardHeight);
             // every iteration, move startingX by my desired grid
             startingX += 136; // x because this inner loop handles columns
@@ -38,4 +36,6 @@ function draw() {
         startingX = 24; // reset X so new rows line up at the "origin" again
     }
 }
-// .wav or .mp4 file if you want a sound when you interact w your cards
+
+// .wav or .mp4 file if you want a sound when you interact w your cards... 'good grief', 'teacher wah wah', 'snoopy/woodstock laughing'....?
+// can I use a specific font in my p5js? there's a comic font that could be fun to keep in theme
