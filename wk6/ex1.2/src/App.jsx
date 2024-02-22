ae193d51f244b4c8001fb8cc0ff0afefb17ee4f9
C Jones <cjones013@gmail.com>
2024-02-21 22:52:41 -0600

    wk6 ex1.2 start

---
+++ wk6/ex1.2/src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+
+/* function App() {
+  const happy = true;
+  return (
+    <>
+      Hi this is a React App that I made!
+      {happy && (<div>Yay!</div>)}
+    </>
+  )
+} */
+
+function App() {
+  const greeting = 'Hello, world!'
+  const thisString = 'this'
+  const thisArray = ['THIS, &', 'THAT, and', 'ANOTHER THING!']
+  const oneMoreString = '... and one more!'
+  return (
+    <>
+      <div class="header">
+        <h1>{greeting}</h1>
+        <h2>Get a load of... <a href="https://www.google.com" target="_blank">{thisString}!</a></h2>
+      </div>
+      <div>
+        <h3>And...! (And!!)</h3>
+        <div>
+          <p>(wait for it...)</p>
+          <div class="card">
+            {thisArray.map((item) => {
+              return (<p>{item}</p>)
+            })}
+          </div>
+          <p class="emphatic">{oneMoreString}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
