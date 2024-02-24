952d1882e45c2bb7ae10711fcc5dd3afff824ea3
C Jones <cjones013@gmail.com>
2024-02-24 15:10:00 -0600

    wk6 assignment typo fix

--- wk6/assignments/src/App.jsx
+++ wk6/assignments/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     "Fast llamas are often referred to as 'llamaghinis'",
     "Llamas are proficient story tellers",
     "A normal llama pregnancy lasts for an average of 11 to 11.5 months",
-    "LLamas eat 7 meals a day, like their good friends the hobbits",
+    "Llamas eat 7 meals a day, like their good friends the hobbits",
     "Llamas are great pack animals, but if you try to overload them with too much weight, they will likely lie down or simply refuse to move",
     "The average llama is between 5 feet 6 inches and 5 feet 9 inches tall",
     "LLama poop has almost no oder and makes for an eco-friendly fertilizer"
