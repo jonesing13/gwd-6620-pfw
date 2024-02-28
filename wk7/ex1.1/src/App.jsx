f5cfe983fb4fe0f75142cbd6d7c04335f66559a8
C Jones <cjones013@gmail.com>
2024-02-27 20:21:05 -0800

    wk7 ex1.1 adding images to folders

--- wk7/ex1.1/src/App.jsx
+++ wk7/ex1.1/src/App.jsx
@@ -1,8 +1,23 @@
-function App() {
+import Camelid from './Camelid'
+import './App.jsx'
 
+function App() {
+  const camelidArray = [
+    {name: "Llama", funFact: "Fast llamas are referred to as 'llamaghinis'", img: ""},
+    {name: "Alpaca", funFact: "Alpacas prefer to sing alpacapella", img: ""}
+  ]
   return (
     <div>
-
+      <Camelid
+        name={camelidArray[0].name}
+        funFact={camelidArray[0].funFact}
+        //img={}
+      />
+      <Camelid
+        name={camelidArray[1].name}
+        funFact={camelidArray[1].funFact}
+        //img={}
+      />
     </div>
   )
 }
---
+++ wk7/ex1.1/src/Camelid.jsx
@@ -0,0 +1,11 @@
+function Camelid({name, funFact, img}) {
+
+    return (
+        <div>
+            <h1>{name}</h1>
+            <p>{funFact}</p>
+        </div>
+    );
+}
+
+export default Camelid;
