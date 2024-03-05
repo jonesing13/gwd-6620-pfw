5bafe46e160f2e4fcf4fa8ddcb86195711e09c3d
C Jones <cjones013@gmail.com>
2024-03-04 23:48:01 -0600

    adding files for wk8 assignment

--- wk8/ex8.2/src/App.jsx
+++ wk8/ex8.2/src/App.jsx
@@ -7,6 +7,42 @@ function App() {
   // useState to indicate the page header
   const [pageHeader, setPageHeader] = useState("Animals from the Hundred Acre Wood");
 
+/*  const animalList = [{
+    name: "Winnie the Pooh",
+    img: "./bonsai-tree_product_image_267x400.jpg",
+  },
+  {
+    name: "Piglet",
+    img: "./bonsai-tree_product_image_267x400.jpg",
+  },
+  {
+    name: "Eeyore",
+    img: "./bonsai-tree_product_image_267x400.jpg",
+  },
+  {
+    name: "Tigger",
+    img: "./bonsai-tree_product_image_267x400.jpg",
+  },
+  {
+    name: "Owl",
+    img: "./bonsai-tree_product_image_267x400.jpg",
+  },
+  {
+    name: "Rabbit",
+    img: "./bonsai-tree_product_image_267x400.jpg",
+  },
+  {
+    name: "Kanga",
+    img: "./bonsai-tree_product_image_267x400.jpg",
+  },
+  {
+    name: "Little Roo",
+    img: "./bonsai-tree_product_image_267x400.jpg",
+  },
+]
+Error: Objects are not valid as a React child (found: object with keys {name, img}). If you meant to render a collection of children, use an array instead. 
+*/
+
   const [animal, setAnimal] = useState([
     "Winnie the Pooh",
     "Eeyore",
@@ -14,7 +50,8 @@ function App() {
     "Piglet",
     "Kanga",
     "Little Roo",
-    "Owl"
+    "Owl",
+    "Rabbit"
   ]);
 
   // delete button functionality--remove animal from the list and return updated list
