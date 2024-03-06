a5e9d43e3eb8ed221c7f6b85a856c9ebc6f518bb
C Jones <cjones013@gmail.com>
2024-03-05 20:58:58 -0600

    annotations for ex8.1 w anonymous functions

--- wk8/ex8.1copy/src/Animal.jsx
+++ wk8/ex8.1copy/src/Animal.jsx
@@ -1,5 +1,6 @@
-import PropTypes from "prop-types"; //technically this is built in when you use Typescript
+import PropTypes from "prop-types";
 
+// return an image url when Animal is called
 export default function Animal ({ url }) {
     return (
         <>
@@ -8,12 +9,6 @@ export default function Animal ({ url }) {
     )
 }
 
-// use terminal command: 'npm install prop-types --save' will add the prop-types right into your pacakage.json file under "dependencies" to share w other folks accessing your code
-// otherwise, add -dev to the command to add to the devDependencies on your package.json file, if you're like the only one using the file I think. "I'm only concerned with this for dev purposes"
-
-// now you can use propTypes 
-
 Animal.propTypes = {
     url: PropTypes.string.isRequired,
 }
-
