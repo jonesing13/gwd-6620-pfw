3f52a4523f7aab21080f0dedaaf302431620b750
C Jones <cjones013@gmail.com>
2024-03-05 20:57:51 -0600

    exercise 8.1 'show me the camelid' using anonymous functions

--- wk8/ex8.1copy/src/App.jsx
+++ wk8/ex8.1copy/src/App.jsx
@@ -4,34 +4,23 @@ import llama from './assets/llama_768x512.jpg'
 import Animal from './Animal';
 import './App.css'
 
-
-/*
-
-INSTRUCTIONS
-
-Either using a new sandbox in codesandbox.io or by creating a new React app with "create vite@latest" create a simple application with one image and with two buttons. One button should say "Alpaca" and the other should say "Llama". Use the "useState" hook to set a state variable to the image for the Llama picture. Use this state variable to set the image in an IMG tag. Each button should have an onClick action to set the state variable to the respective camelid. The end result will be an app that shows either a llama or an alpaca depending on which button you have selected last.
-
-*/
-
 export default function App() {
-
+  // on landing, show llama
   const [animal, setAnimal] = useState(llama)
 
+  // on button click (depending on button) show the related imagery
   return (
     <>
-      <div>
-        <Animal url={llama} />
-      </div>
       <h1>Show me the Camelid</h1>
+      <Animal url={animal} />
+      
       <div>
         <button onClick={() => {
-          console.log("you clicked llama");
           setAnimal(llama);
         }}>
           Llama
         </button>
         <button onClick={() => {
-          console.log("you clicked alpaca");
           setAnimal(alpaca);
         }}>
           Alpaca
