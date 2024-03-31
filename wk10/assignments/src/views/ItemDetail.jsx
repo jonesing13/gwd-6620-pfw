422b7bf4a4ea2da9e2b8bacc7e9287e1c9807f30
C Jones <cjones013@gmail.com>
2024-03-30 22:07:31 -0500

    wk10 assignment CSS (why is there white at the bottom? why does 'retired' class leave container?)

--- wk10/assignments/src/views/ItemDetail.jsx
+++ wk10/assignments/src/views/ItemDetail.jsx
@@ -1,5 +1,7 @@
 import PropTypes from "prop-types"
 import { useParams, Link } from "react-router-dom"
+import Footer from "../Footer/Footer"
+import "./ItemDetail.css"
 
 export function ItemDetail({ data }) {
     const { id } = useParams()
@@ -8,22 +10,35 @@ export function ItemDetail({ data }) {
     console.log("lego", selectedLego)
 
     return (
-        <div>
-            <nav>
+        <div className="views-detail">
+            <nav className="title">
                 <Link to ="/">Return to List</Link>
+
+                <h1>{selectedLego.name}</h1>
             </nav>
 
-            <h1>{selectedLego.name}</h1>
-            <img src={selectedLego.image} alt={selectedLego.name} />
-            <p>
-                {selectedLego.description}
-            </p>
-            <p>
-                Rating: {selectedLego.rating}
-            </p>
-            <p>
-                {selectedLego.retired && (<p>This set is retired, and no longer sold by LEGO<sup>&reg;</sup></p>)}
-            </p>
+            <div className="lego-detail">
+                {/* TODO: "retired" style is.... taking itself out of the container of the div??? 
+                
+                also... can I add the "dead" image to the banner?
+                */}
+                {selectedLego.retired && (<p  className="retired">This set is retired, and no longer sold by LEGO<sup>&reg;</sup></p>)}
+
+                <img src={selectedLego.image} alt={selectedLego.name} />
+
+                <ul>
+                    <li>
+                        {selectedLego.description}
+                    </li>
+                    <li>
+                        <strong>Rating:</strong> {selectedLego.rating}
+                    </li>
+                </ul>
+            </div>
+
+            <div className="uncontained-link">
+                
+            </div>
         </div>
     )
 }
